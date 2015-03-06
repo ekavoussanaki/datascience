93910a46582d7e138e8d1a38c0e31ef5e3afc33b
moijoune <ekavoussanaki@users.noreply.github.com>
2015-03-06 10:52:42 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,6 +4,6 @@ data.subset2<-subset(data,data$Date==c("2/2/2007"))
 data.subset3<-subset(data,data$Date==c("3/2/2007"))
 data.subset<-rbind(data.subset1,data.subset2,data.subset3)
 
-#png("plot1.png", width=480, height=480)
+png("plot1.png", width=480, height=480)
 hist(as.numeric(data.subset$Global_active_power[1:2880]),col="red",xlab="Global Active Power (kilowatts)",main="Global Active Power",ylim=c(0,1300))
-#dev.off()
+dev.off()
